d4bf38b93c6fe57b04c9528b480b0bb646267240
root <517433351@qq.com>
2019-10-11 23:43:01 +0800

    第六次提交  进行list遍历

--- git1/src/com/yunjian/demo/gitdemo1.java
+++ git1/src/com/yunjian/demo/gitdemo1.java
@@ -1,10 +1,27 @@
 package com.yunjian.demo;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class gitdemo1 {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
         System.out.println("git2");
         System.out.println("git2");
+
+
+        List s = new ArrayList();
+        s.add("1");
+        s.add("2");
+        s.add("3");
+        s.add("4");
+        s.add("5");
+
+        for (int i = 0; i < s.size(); i++) {
+
+        }
+        System.out.println(s.get(1));
     }
 }
